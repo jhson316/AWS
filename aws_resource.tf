b2b68fc79e9460bd5776157948361451570138e0
jhson316 <fkwnaak@gmail.com>
2022-06-08 01:05:09 +0000

    Change Secu Group

--- aws_resource.tf
+++ aws_resource.tf
@@ -37,10 +37,18 @@ resource "aws_default_vpc" "default_vpc" {
 
 resource "aws_default_security_group" "default" {
   vpc_id = aws_default_vpc.default_vpc.id
-  ingress = {
-    cidr_blocks = [0.0.0.0/0]
-    protocol = -1
+  
+  ingress {
+    protocol  = -1
+    self      = true
     from_port = 0
-    to_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
