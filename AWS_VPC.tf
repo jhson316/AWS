2b982f4eb90c664f7be7185446bc98ccb1d0d1c1
jhson316 <fkwnaak@gmail.com>
2022-06-14 09:16:04 +0000

    Change ACL

--- AWS_VPC.tf
+++ AWS_VPC.tf
@@ -19,21 +19,21 @@ resource "aws_internet_gateway" "igw" {
 # network acl
 resource "aws_default_network_acl" "vpc_network_acl" {
     default_network_acl_id = aws_vpc.VPC_test.default_network_acl_id
-    egress {
+    ingress {
         protocol   = "tcp"
+        rule_no    = 100
+        action     = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port  = 22
+        to_port    = 22
+    }
+    egress {
+        protocol   = "all"
         rule_no    = 100
         action     = "allow"
         cidr_block = "0.0.0.0/0"
         from_port  = 0
         to_port    = 65535
-    }
-    ingress {
-        protocol   = "-1"
-        rule_no    = 100
-        action     = "allow"
-        cidr_block = "0.0.0.0/0"
-        from_port  = 0
-        to_port    = 0
     }
     tags = {
         Name = "network acl"
