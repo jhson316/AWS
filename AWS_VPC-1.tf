8fc940bdbfc51106c3d4fa230960091f565066c0
jhson316 <fkwnaak@gmail.com>
2022-06-14 08:24:33 +0000

    pri TEST2

--- AWS_VPC-1.tf
+++ AWS_VPC-1.tf
@@ -6,3 +6,12 @@ resource "aws_vpc" "VPC_test-1" {
     Name = "VPC Test-1"
   }
 }
+
+# Internet gateway 생성
+resource "aws_internet_gateway" "igw-1" {
+  vpc_id = aws_vpc.VPC_test-1.id
+
+  tags = {
+    Name = "Internet Gateway-1"
+  }
+}
--- AWS_VPC-2.tf
+++ AWS_VPC-2.tf
@@ -6,3 +6,12 @@ resource "aws_vpc" "VPC_test-2" {
     Name = "VPC Test-2"
   }
 }
+
+# Internet gateway 생성
+resource "aws_internet_gateway" "igw-2" {
+  vpc_id = aws_vpc.VPC_test-2.id
+
+  tags = {
+    Name = "Internet Gateway-2"
+  }
+}
