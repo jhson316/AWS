d0fdbf16788154c3a10a6d016d9de8e1ae4141ee
jhson316 <fkwnaak@gmail.com>
2022-06-14 05:38:36 +0000

    VPC TEST10

---
+++ AWS_public.tf
@@ -0,0 +1,50 @@
+# Public망 Subnet 생성
+resource "aws_subnet" "pub_sub1" {
+  vpc_id     = aws_vpc.VPC_test.id
+  cidr_block = "193.17.0.0/24"
+  tags = {
+    "Name" = "VPC Test pub_sub1"
+  }
+}
+
+resource "aws_subnet" "pub_sub2" {
+  vpc_id     = aws_vpc.VPC_test.id
+  cidr_block = "193.17.1.0/24"
+  tags = {
+    "Name" = "VPC Test pub_sub2"
+  }
+}
+
+# Internet gateway 생성
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.VPC_test.id
+
+  tags = {
+    Name = "Internet Gateway"
+  }
+}
+
+# Public망 Default GW 라우팅 설정
+resource "aws_default_route_table" "public_rt" {
+  default_route_table_id = aws_vpc.VPC_test.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public route table"
+  }
+}
+
+# Public망 Subnet에 Default Gateway 라우팅 매칭
+resource "aws_route_table_association" "public_rta_a" {
+  subnet_id      = aws_subnet.pub_sub1.id
+  route_table_id = aws_default_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_rta_b" {
+  subnet_id      = aws_subnet.pub_sub2.id
+  route_table_id = aws_default_route_table.public_rt.id
+}
