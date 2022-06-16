fe0e5577fcd72e122e071ea786b8edcb66bcbdf4
jhson316 <fkwnaak@gmail.com>
2022-06-16 02:17:36 +0000

    SG Test

--- AWS_VPC.tf
+++ AWS_VPC.tf
@@ -39,3 +39,27 @@ resource "aws_default_network_acl" "vpc_network_acl" {
         Name = "network acl"
     }
 }
+
+# security group
+resource "aws_default_security_group" "default_sg" {
+    vpc_id = aws_vpc.VPC_test.id
+
+    ingress {
+        protocol    = "tcp"
+        from_port = 0
+        to_port   = 65535
+        cidr_blocks = [aws_vpc.vpc.cidr_block]
+    }
+
+    egress {
+        protocol    = "-1"
+        from_port   = 0
+        to_port     = 0
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "default_sg"
+        Description = "default security group"
+    }
+}
