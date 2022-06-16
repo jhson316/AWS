8c1f60da121c6b534616ed7dd1b20b6db305d68f
jhson316 <fkwnaak@gmail.com>
2022-06-16 04:56:44 +0000

    SG Test5

--- AWS_public.tf
+++ AWS_public.tf
@@ -14,7 +14,7 @@ resource "aws_route_table" "pub_route_table" {
     gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
-    Name = "Public Route Table"
+    Name = "VPC TEST Public Route Table"
   }
 }
 
